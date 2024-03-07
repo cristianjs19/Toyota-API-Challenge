26de1cd3f45ad2c80ffdadfa06f8e9fd06c251c3
Cristian <cristianjs19@gmail.com>
2024-03-07 00:49:54 -0300

    Create models

--- toyota_api/vehicles/models.py
+++ toyota_api/vehicles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+TYPES = (
+    (1, 'Autos'),
+    (2, 'Pickups y Comerciales'),
+    (3, 'SUVs y Crossovers')
+)
+
+class Characteristic(models.Model):
+    title = models.CharField(max_length=150, blank=True, default='')
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=False, blank=True)
+
+
+class Vehicle(models.Model):
+    model_name = models.CharField(max_length=100, blank=False, default='')
+    subtitle = models.CharField(max_length=150, blank=True, default='')
+    description = models.TextField(null=True, blank=True)
+    type = models.IntegerField(choices=TYPES, default=1)
+    year = models.IntegerField(blank=False, default=0)
+    price = models.IntegerField(blank=False, default=0)
+    image = models.ImageField(upload_to='images/', null=False, blank=True)
+    active = models.BooleanField(default=True)
+    characteristics =  models.ForeignKey(Characteristic, null=True, blank=True, on_delete=models.SET_NULL)
+
